c173496e3976769811abe21b04ea04f1cce1cbd8
Varshithvhegde <varshithvh@gmail.com>
2022-09-03 10:45:15 +0530

    Added

--- DaeonThread.java
+++ DaeonThread.java
@@ -15,7 +15,6 @@ public class DaeonThread extends Thread{
         t1.setDaemon(true);//now t1 is daemon thread
         t1.start();//starting threads
         t2.start();
-        // t3.setDaemon(true);
         t3.start();
     }
     
