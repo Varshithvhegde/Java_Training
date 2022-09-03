38ad223db44044ea2c9c6e42d816429ec2ffc081
Varshithvhegde <varshithvh@gmail.com>
2022-09-03 10:01:40 +0530

    Added new multi threading

---
+++ Thread_Multi.java
@@ -0,0 +1,21 @@
+public class Thread_Multi extends Thread{
+    public void run(){
+        for(int i=1;i<5;i++){
+            try{
+                Thread.sleep(500);
+            }catch(InterruptedException e){
+                System.out.println(e);
+            }
+            System.out.println(i);
+        }
+    }
+    public static void main(String args[]){
+        Thread_Multi t1=new Thread_Multi();
+        Thread_Multi t2=new Thread_Multi();
+       // Thread_Multi t3=new Thread_Multi();
+        t1.start();
+        t2.start();
+        //t3.start();
+    }
+    
+}
