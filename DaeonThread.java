12b3b492e92a0a370ae1f55ece79d1ca79f308c1
Varshithvhegde <varshithvh@gmail.com>
2022-09-03 10:44:35 +0530

    Added

---
+++ DaeonThread.java
@@ -0,0 +1,22 @@
+public class DaeonThread extends Thread{
+    public void run(){
+        if(Thread.currentThread().isDaemon()){
+            System.out.println("Daemon thread is running...");
+        }
+        else{
+            System.out.println("User thread is running...");
+        }
+    }
+    public static void main(String[] args){
+        DaeonThread t1=new DaeonThread();
+        DaeonThread t2=new DaeonThread();
+        DaeonThread t3=new DaeonThread();
+
+        t1.setDaemon(true);//now t1 is daemon thread
+        t1.start();//starting threads
+        t2.start();
+        // t3.setDaemon(true);
+        t3.start();
+    }
+    
+}
