cb1de865277196459029b0c45756f23c18f93c47
Varshithvhegde <varshithvh@gmail.com>
2022-09-04 19:37:27 +0530

    Subset of array is wriiten

---
+++ subset_array.java
@@ -0,0 +1,32 @@
+public class subset_array{
+
+    public static void main(String args[]){
+        int arr[]={1,2,3};
+        int n=arr.length;
+        int sum=0;
+        for(int i=0;i<(1<<n);i++){
+            int temp=0;
+            for(int j=0;j<n;j++){
+              
+                
+                if((i&(1<<j))!=0){
+                    
+                    if(j == 0){
+                        temp = arr[j];
+                    }
+                    else{
+                        temp = arr[j-1] ;
+                    }
+                
+                    temp=temp&arr[j];
+                    System.out.print(arr[j]+" ");
+
+
+                }
+            }
+            sum+=temp;
+            System.out.println();
+        }
+        System.out.println(sum);
+    }
+}
